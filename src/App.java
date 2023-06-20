a15ebaf9499e2431351e68f1ed583e679d9b1734
Gilson Damaso <gilson.damaso@gmail.com>
2023-06-20 13:26:30 -0300

    ProjetoCrudBasico

---
+++ src/App.java
@@ -0,0 +1,21 @@
+import Classes.Pessoa;
+import Control.ControlPessoaArray;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        Pessoa p1 = new Pessoa(10, "GILSON", 39 );
+        Pessoa p2 = new Pessoa(11, "MIGUEL", 9 );
+        Pessoa p3 = new Pessoa(12, "POLY", 37 );
+        Pessoa p4 = new Pessoa(13, "POLY", 37 );
+        Pessoa p5 = new Pessoa(14, "POLY", 37 );
+        ControlPessoaArray ctrlPessoa = new ControlPessoaArray();
+        
+        ctrlPessoa.adicionarPessoaVazio(10, p1,p2,p3);
+        //ctrlPessoa.adicionarPessoa(2,p1, p2);
+
+        ctrlPessoa.atualizarPessoa(new Pessoa(12, "POLYANA", 38));
+
+        ctrlPessoa.listarPessoa();
+    }
+}
